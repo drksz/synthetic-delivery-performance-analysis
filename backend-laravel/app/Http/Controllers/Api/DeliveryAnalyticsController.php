08eb29eb22c13ee4deab24a022dfecee23b9e0f4
Derek P <pajarin.derek.b@gmail.com>
2026-06-19 11:02:35 +0800

    add caching to API controller methods

--- backend-laravel/app/Http/Controllers/Api/DeliveryAnalyticsController.php
+++ backend-laravel/app/Http/Controllers/Api/DeliveryAnalyticsController.php
@@ -5,170 +5,223 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class DeliveryAnalyticsController extends Controller
 {
+    // give 5 minutes time-to-live for all requests
+    private const TTL = 300;
+
     public function kpis() {
         
-        $result = DB::select("
-            SELECT 
-                COUNT(*) AS total_deliveries,
-                ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY delay)::numeric,3) AS median_delay_mins,
+        $result = Cache::remember(
+            'kpis',
+            self::TTL,
+            function() {
+                return DB::select("
+                    SELECT 
+                        COUNT(*) AS total_deliveries,
+                        ROUND(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY delay)::numeric,3) AS median_delay_mins,
 
-                ROUND(
-                    (SUM(CASE WHEN on_time = true THEN 1 ELSE 0 END) * 100.0)
-                    / COUNT(*)::numeric, 2
-                ) AS on_time_percentage
-    
-            FROM delivery_records; 
-        ");
+                        ROUND(
+                            (SUM(CASE WHEN on_time = true THEN 1 ELSE 0 END) * 100.0)
+                            / COUNT(*)::numeric, 2
+                        ) AS on_time_percentage
 
+                    FROM delivery_records; 
+                ");
+            }
+        );
+        
         return response()->json($result);
     }
 
     public function weatherStats() {
 
-        $result = DB::select("
-            SELECT
-                weather,
-                ROUND(
-                    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY delay)::numeric, 2
-                ) AS median_delay_mins,
-    
-                ROUND(
-                    AVG(est_veh_spd)::numeric, 2
-                ) AS est_vehicle_speed_kmh
-    
-            FROM delivery_records
-            GROUP BY weather;
-        ");
+        $result = Cache::remember(
+            'weather-stats',
+            self::TTL,
+            function() {
+                return DB::select("
+                    SELECT
+                        weather,
+                        ROUND(
+                            PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY delay)::numeric, 2
+                        ) AS median_delay_mins,
+            
+                        ROUND(
+                            AVG(est_veh_spd)::numeric, 2
+                        ) AS est_vehicle_speed_kmh
+            
+                    FROM delivery_records
+                    GROUP BY weather;
+                ");
+            }
+        );
 
         return response()->json($result);
     }
 
     public function vehicleStats() {
 
-        $result = DB::select("
-            WITH row_count AS (
-                SELECT COUNT(*) AS total_rows
-                FROM delivery_records
-            )
+        $result = Cache::remember(
+            'vehicle-stats',
+            self::TTL,
+            function() {
+                return DB::select("
+                    WITH row_count AS (
+                        SELECT COUNT(*) AS total_rows
+                        FROM delivery_records
+                    )
 
-            SELECT 
-                dr.vehicle_type,
-                COUNT(*) AS \"count\",
-                ROUND(100.0 * COUNT(*) / rc.total_rows::numeric, 2) AS proportion
+                    SELECT 
+                        dr.vehicle_type,
+                        COUNT(*) AS \"count\",
+                        ROUND(100.0 * COUNT(*) / rc.total_rows::numeric, 2) AS proportion
 
-            FROM delivery_records AS dr
-            CROSS JOIN row_count AS rc
-            GROUP BY dr.vehicle_type, rc.total_rows
-            ORDER BY proportion DESC;
-        
-        ");
+                    FROM delivery_records AS dr
+                    CROSS JOIN row_count AS rc
+                    GROUP BY dr.vehicle_type, rc.total_rows
+                    ORDER BY proportion DESC;
+                
+                ");
+            }
+        );
 
         return response()->json($result);
     }
 
     public function priorityStats() {
 
-        $result = DB::select("
-            WITH row_count AS (
-                SELECT COUNT(*) AS total_rows
-                FROM delivery_records
-            )
+        $result = Cache::remember(
+            'priority-stats',
+            self::TTL,
+            function() {
+                return DB::select("
+                    WITH row_count AS (
+                        SELECT COUNT(*) AS total_rows
+                        FROM delivery_records
+                    )
 
-            SELECT
-                dr.priority,
-                ROUND(100.0 * COUNT(*) / rc.total_rows::numeric, 2) AS proportion,
-                COUNT(*) FILTER(WHERE on_time = true) AS on_time_count,
-                COUNT(*) FILTER(WHERE on_time = false) AS late_count,
+                    SELECT
+                        dr.priority,
+                        ROUND(100.0 * COUNT(*) / rc.total_rows::numeric, 2) AS proportion,
+                        COUNT(*) FILTER(WHERE on_time = true) AS on_time_count,
+                        COUNT(*) FILTER(WHERE on_time = false) AS late_count,
 
-                ROUND(
-                    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dr.delay)::numeric, 2
-                ) AS median_delay_mins
+                        ROUND(
+                            PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dr.delay)::numeric, 2
+                        ) AS median_delay_mins
 
-            FROM delivery_records AS dr
-            CROSS JOIN row_count AS rc
-            GROUP BY dr.priority, rc.total_rows;
-        ");
+                    FROM delivery_records AS dr
+                    CROSS JOIN row_count AS rc
+                    GROUP BY dr.priority, rc.total_rows;
+                ");
+            }
+        );
 
         return response()->json($result);
     }
 
     public function weekdayDelay() {
 
-        $result = DB::select("
-            SELECT 
-              dr.weekday,
-            ROUND(  
-                PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dr.delay)::numeric, 2
-            )AS median_delay_mins
+        $result = Cache::remember(
+            'weekday-delay',
+            self::TTL,
+            function() {
+                return DB::select("
+                    SELECT 
+                    dr.weekday,
+                    ROUND(  
+                        PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY dr.delay)::numeric, 2
+                    )AS median_delay_mins
 
-            FROM delivery_records AS dr
-            GROUP BY dr.weekday 
-            ORDER BY (
-                CASE dr.weekday
-                    WHEN 'Monday' THEN 1
-                    WHEN 'Tuesday' THEN 2
-                    WHEN 'Wednesday' THEN 3
-                    WHEN 'Thursday' THEN 4
-                    WHEN 'Friday' THEN 5
-                    WHEN 'Saturday' THEN 6
-                    WHEN 'Sunday' THEN 7
-                    ELSE 0 END
-                );
-        ");
+                    FROM delivery_records AS dr
+                    GROUP BY dr.weekday 
+                    ORDER BY (
+                        CASE dr.weekday
+                            WHEN 'Monday' THEN 1
+                            WHEN 'Tuesday' THEN 2
+                            WHEN 'Wednesday' THEN 3
+                            WHEN 'Thursday' THEN 4
+                            WHEN 'Friday' THEN 5
+                            WHEN 'Saturday' THEN 6
+                            WHEN 'Sunday' THEN 7
+                            ELSE 0 END
+                        );
+                ");
+            }
+        );
 
         return response()->json($result);
     }
 
     public function weekTotalDelay() {
-    
 
-    $result = DB::select("
-        SELECT
-        DATE_TRUNC('week', date) AS week_start,
-        ROUND(SUM(delay)::numeric, 2) AS total_delay_mins
+        $result = Cache::remember(
+            'week-total-delay',
+            self::TTL,
+            function() {
+                return DB::select("
+                    SELECT
+                    DATE_TRUNC('week', date) AS week_start,
+                    ROUND(SUM(delay)::numeric, 2) AS total_delay_mins
 
-        FROM delivery_records
-        GROUP BY week_start 
-        ORDER BY week_start;
-    ");
+                    FROM delivery_records
+                    GROUP BY week_start 
+                    ORDER BY week_start;
+                ");
+            }
+        );
 
-    return response()->json($result);
-}
+        return response()->json($result);
+    }
+
+
 
     public function monthTotalDelay() {
 
-        $result = DB::select("
-            SELECT
-            DATE_TRUNC('month', date) AS month_start,
-            ROUND(SUM(delay)::numeric, 2) AS total_delay_mins
+        $result = Cache::remember(
+            'month-total-delay',
+            self::TTL,
+            function() {
+                return DB::select("
+                    SELECT
+                    DATE_TRUNC('month', date) AS month_start,
+                    ROUND(SUM(delay)::numeric, 2) AS total_delay_mins
 
-            FROM delivery_records
-            GROUP BY month_start 
-            ORDER BY month_start;
-        ");
+                    FROM delivery_records
+                    GROUP BY month_start 
+                    ORDER BY month_start;
+                ");
+            }
+        );
 
         return response()->json($result);
     }
 
     public function ratingSummary() {
 
-        $result = DB::select("
-            SELECT
-                r.rating_name,
-                ROUND(AVG(r.vals)::numeric, 2) AS average
-            FROM delivery_records, 
-            LATERAL (
-                VALUES
-                    ('Attitude', attitude),
-                    ('Package Care', pkg_care),
-                    ('Responsiveness', responsiveness),
-                    ('Delivery Speed', delivery_spd)
-                    ) AS r(rating_name, vals)
-            GROUP BY r.rating_name;
-        ");
+        $result = Cache::remember(
+            'rating-summary',
+            self::TTL,
+            function() {
+                return DB::select("
+                    SELECT
+                        r.rating_name,
+                        ROUND(AVG(r.vals)::numeric, 2) AS average
+                    FROM delivery_records, 
+                    LATERAL (
+                        VALUES
+                            ('Attitude', attitude),
+                            ('Package Care', pkg_care),
+                            ('Responsiveness', responsiveness),
+                            ('Delivery Speed', delivery_spd)
+                            ) AS r(rating_name, vals)
+                    GROUP BY r.rating_name;
+                ");
+            }
+        );
 
         return response()->json($result);
     }
